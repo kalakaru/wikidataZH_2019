4749518df48f0fff1a709b5f437de159732d9a28
Kalakaru <Kalakaru@local>
2019-12-03 10:32:35 +0000

    improve data_imports

--- data_imports.py
+++ data_imports.py
@@ -6,6 +6,31 @@ import shutil
 from SPARQLWrapper import SPARQLWrapper, JSON, XML
 import logging
 
+def import_kantonZH_api():
+    URL = "https://www.web.statistik.zh.ch:8443/gp/GP?type=EXPORT&indikatoren=133&raumtyp=1&export=csv"
+
+    r = requests.get(url=URL)
+    try:
+        os.mkdir('data')
+    except OSError as ex:
+        print(ex)
+
+    open('data/dataKt.zip', 'wb').write(r.content)
+    zfile = zipfile.ZipFile('data/dataKt.zip')
+    with zipfile.ZipFile('data/dataKt.zip', 'r') as f:
+        names = f.namelist()
+
+    df = pd.read_csv(zfile.open(names[0]), sep=';')
+    df = df.iloc[:, :-1]
+    df = pd.melt(df, id_vars=[df.columns[0],df.columns[1]], value_vars=df.columns[2:])
+    df = df.rename(columns={"value": "population", "variable":"date"})
+    df['date'] = df['date'].astype('str')+ '-12-31'
+
+    zfile.close()
+    shutil.rmtree('data')
+    logging.info('Import Kanton data: extracted {0} entries successful'.format(df['BFS_NR'].count()))
+    return df
+
 def import_cityZH_api():
     def _query_opendata_zurich(resource_id, parse_record):
         result = requests.get(
@@ -33,33 +58,7 @@ def import_cityZH_api():
     _apply_wikidata_mapping(mapping_rows, population_rows)
     return pd.DataFrame(population_rows)
 
-
-def import_kantonZH_api():
-    URL = "https://www.web.statistik.zh.ch:8443/gp/GP?type=EXPORT&indikatoren=133&raumtyp=1&export=csv"
-
-    r = requests.get(url=URL)
-    try:
-        os.mkdir('data')
-    except OSError as ex:
-        print(ex)
-
-    open('data/dataKt.zip', 'wb').write(r.content)
-    zfile = zipfile.ZipFile('data/dataKt.zip')
-    with zipfile.ZipFile('data/dataKt.zip', 'r') as f:
-        names = f.namelist()
-
-    df = pd.read_csv(zfile.open(names[0]), sep=';')
-    df = df.iloc[:, :-1]
-    df = pd.melt(df, id_vars=[df.columns[0],df.columns[1]], value_vars=df.columns[2:])
-    df = df.rename(columns={"value": "population", "variable":"date"})
-    df['date'] = df['date'].astype('str')+ '-12-31'
-
-    zfile.close()
-    shutil.rmtree('data')
-    logging.info('Import Kanton data: extracted {0} entries successful'.format(df['BFS_NR'].count()))
-    return df
-
-def import_geoadmin_wikidata_kt():
+def import_geoadminwikidata_kantonZH():
     """
     Validation of BFS number: Checks which BFS numbers are active between geo.admin and wikidata
     :return:
@@ -86,13 +85,11 @@ def import_geoadmin_wikidata_kt():
     ?Municipality schema:validUntil ?validuntil .
     ?Municipality gn:parentADM1 ?InCanton .
     ?InCanton schema:name ?CantonName .
-    #?Municipality geo:hasGeometry ?Geometry .
-    #?Geometry geo:asWKT ?WKT .
-    
+     
     FILTER (now()<=?validuntil)
     FILTER (?CantonName = 'Zürich')
 
-    {SELECT DISTINCT (xsd:integer(?bfsWD) AS ?bfs)?wikidata_id ?Image WHERE {
+    {SELECT DISTINCT (xsd:integer(?bfsWD) AS ?bfs)?wikidata_id WHERE {
     SERVICE <https://query.wikidata.org/bigdata/namespace/wdq/sparql>
 
     {
@@ -101,10 +98,37 @@ def import_geoadmin_wikidata_kt():
     #OPTIONAL { ?wikidata_id wdt:P18 ?Image. } .
     }}}}
     """
-    
-def import_wikidata_kt():
+
+    def get_results(endpoint_url, query):
+        sparql = SPARQLWrapper(
+            endpoint_url,
+            agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36")
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        return sparql.query().convert()
+
+    results = get_results(endpoint_url, query)
+    result = results["results"]["bindings"]
+
+    population = []
+    for p in result:
+        mon = {'Name': p['Name']['value'],
+               'bfs': p['bfs']['value'],
+               'wikidata_id': p['wikidata_id']['value'].replace('http://www.wikidata.org/entity/', '')
+               }
+        population.append(mon)
+
+    pop = pd.DataFrame(population)
+    pop['bfs'] = pop['bfs'].astype(int)
+
+    return pop
+
+_population_resource_id = "570f006e-2f2a-4b1f-9233-c4916c753475"
+_mapping_resource_id = "0090f2ed-1df9-4953-9561-5d413fd74758"
+
+def import_wikidata_kantonZH():
     """
-    Generates a SPARQL query and converts this data to a pandas datadframe
+    Generates a SPARQL query and converts this data to pandas datadframe
     :return: pd.Dataframe['wikidata_id','date','population','qualifier']
     """
     # Q70208 Gemeinde
@@ -112,19 +136,22 @@ def import_wikidata_kt():
     # P1082 Einwohner
     # P585 Zeitmpunkt
     query = """
-        SELECT ?wikidata_id ?bfs_id ?date ?population ?qualifier
-            WHERE
-            {
-
-              ?wikidata_id wdt:P31 wd:Q70208.
-              ?wikidata_id wdt:P131 wd:Q11943.
-              ?wikidata_id p:P1082 ?myvalue.
-              ?wikidata_id wdt:P771 ?bfs_id.
-              ?myvalue pq:P585 ?date.
-              ?myvalue ps:P1082 ?population.
-              ?myvalue wikibase:rank ?qualifier.
-              SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-            }
+        SELECT ?bfs_id ?wikidata_id ?date ?population ?qualifier ?refurl ?refpublisher
+          WHERE
+          {
+          ?wikidata_id wdt:P771 ?bfs_id.
+          ?wikidata_id wdt:P31 wd:Q70208.
+          ?wikidata_id wdt:P131 wd:Q11943.
+          ?wikidata_id p:P1082 ?myvalue.
+          ?myvalue pq:P585 ?date.
+          ?myvalue ps:P1082 ?population.
+          ?myvalue wikibase:rank ?qualifier.
+          ?myvalue prov:wasDerivedFrom ?refnode.
+          OPTIONAL{?refnode pr:P854 ?refurl.}
+          OPTIONAL{?refnode pr:P123 ?refpublisher.}
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+          }
+        order by ?bfs_id ?date
         """
 
     sparql = SPARQLWrapper("https://query.wikidata.org/sparql", agent = "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36")
@@ -142,6 +169,7 @@ def import_wikidata_kt():
             'date': p['date']['value'],
             'population': p['population']['value'],
             'qualifier': p['qualifier']['value'].replace('http://wikiba.se/ontology#', ''),
+            'refURL' : p['refURL']['value'],
         }
         population.append(mon)
 
@@ -152,7 +180,7 @@ def import_wikidata_kt():
     logging.info('Import Wikidata: extracted {0} entries successful'.format(pop['date'].count()))
     return pop
 
-def import_wikidata_qt():
+def import_wikidata_cityZH():
     """
     Generates a SPARQL query and converts this data to pandas datadframe
     :return: pd.Dataframe['wikidata_id','date','population','qualifier']
@@ -197,46 +225,22 @@ def import_wikidata_qt():
     logging.info('Import Wikidata: extracted {0} entries successful'.format(pop['date'].count()))
     return pop
 
-    def get_results(endpoint_url, query):
-        sparql = SPARQLWrapper(endpoint_url,
-                               agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36")
-        sparql.setQuery(query)
-        sparql.setReturnFormat(JSON)
-        return sparql.query().convert()
 
-    results = get_results(endpoint_url, query)
-    result = results["results"]["bindings"]
-
-    population = []
-    for p in result:
-        mon = {'Name': p['Name']['value'],
-               'bfs': p['bfs']['value'],
-               'wikidata_id': p['wikidata_id']['value'].replace('http://www.wikidata.org/entity/', '')
-               }
-        population.append(mon)
-
-    pop = pd.DataFrame(population)
-    pop['bfs'] = pop['bfs'].astype(int)
-
-    return pop
-
-_population_resource_id = "570f006e-2f2a-4b1f-9233-c4916c753475"
-_mapping_resource_id = "0090f2ed-1df9-4953-9561-5d413fd74758"
 
 def main():
     # Database request of city/kanton level
     data_frame = import_cityZH_api().head()
-    print("City of Zurich")
+    print("City API")
     print(data_frame)
     data_frame = import_kantonZH_api().head()
-    print("Kanton data")
+    print("Kanton API ")
     print(data_frame)
     # Network requests
-    data_frame = import_geoadmin_wikidata_kt().head()
+    data_frame = import_geoadminwikidata_kantonZH().head()
     print("Kanton geo.admin.data")
     print(data_frame)
-    data_frame = import_wikidata_kt().head()
-    print("Wikidata for Kanton")
+    data_frame = import_wikidata_kantonZH().head()
+    print("Kanton Wikidata")
     print(data_frame)
 
 
